f1b929537f40624bc3e3173e01b7f9d23a9f8290
Ishu Bansal <ishubansal1400@gmail.com>
2025-01-27 20:50:52 +0530

    show errors on cart page

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,8 +1,10 @@
 import { useDispatch, useSelector } from "react-redux";
 import { setCart } from "../slices/cartSlice";
 import { retryApi } from "../utils";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-const CartItem = ({ item }) => {
+const CartItem = ({ item, showError }) => {
     const dispatch = useDispatch();
 
     const onRemove = async () => {
@@ -11,6 +13,9 @@ const CartItem = ({ item }) => {
             dispatch(setCart(cart));
         }
         catch (err) {
+            const errorMessage = err.response?.data?.message || "Error while removing from cart";
+            const msg = errorMessage.split(':').reverse()[0];
+            showError(msg);
             console.error('Error removing from cart:', err)
         }
     };
@@ -21,6 +26,9 @@ const CartItem = ({ item }) => {
             dispatch(setCart(cart));
         }
         catch (err) {
+            const errorMessage = err.response?.data?.message || "Error while changing the quantity";
+            const msg = errorMessage.split(':').reverse()[0];
+            showError(msg);
             console.error('Error changing quantity in cart:', err);
         }
     };
@@ -41,18 +49,36 @@ const CartItem = ({ item }) => {
 };
 
 export const CartPage = () => {
+    const dispatch = useDispatch();
     const cart = useSelector(state => state.cart.items);
-    const total = useSelector(state => state.cart.items.reduce((acc, item) => (acc + item.quantity * item.dish.price), 0));
+    const total = cart.reduce((acc, item) => (acc + item.quantity * item.dish.price), 0);
+    const [error, setError] = useState("");
+    const showError = (msg) => {
+        setError(msg);
+        setTimeout(() => setError(""), 3000);
+    };
+
+    const onEmptyCart = async () => {
+        try {
+            const cart = await retryApi('delete', `/cart`);
+            dispatch(setCart(cart));
+        }
+        catch (err) {
+            console.error('Error emptying cart:', err)
+        }
+    };
 
     return (
         <div>
             <h1>Cart</h1>
             <ul>
-                {cart.map(item => <CartItem key={item._id} item={item} />)}
+                {cart.map(item => <CartItem key={item._id} item={item} showError={showError} />)}
             </ul>
             <div className="cart-checkout">
+                <Link onClick={onEmptyCart}>Empty Cart</Link>
                 <button><h2>Pay: ₹{total}{" >"}</h2></button>
             </div>
+            {error && <div className="cart-error">{error}</div>}
         </div>
     );
 };
